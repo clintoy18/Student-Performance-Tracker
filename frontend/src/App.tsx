3f898cb0e8724a81f0d70e0ae5dda3be1c307c28
clintoy18 <aicsalonzo@gmail.com>
2025-09-08 23:09:53 +0800

    full setup boilerplate react with asp

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import AuthPage from "./pages/AuthPage";
-
-export default function App() {
-    return (
-        <div className="min-h-screen flex justify-center items-center bg-gray-100">
-            <AuthPage />
-        </div>
-    );
-}
