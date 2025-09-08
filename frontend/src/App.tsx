60c272ea6dba136d0130ee92b357c2eee751afe6
vinceclave <datanaganvince351@gmail.com>
2025-09-09 00:34:02 +0800

    feat[beans]: changes in frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import AuthPage from "./pages/AuthPage";
+
+export default function App() {
+    return (
+        <div className="min-h-screen flex justify-center items-center bg-gray-100">
+            <AuthPage />
+        </div>
+    );
+}
