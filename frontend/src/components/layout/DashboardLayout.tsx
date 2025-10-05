02423058fe44ff807aa6b687cdf4d845569b7f27
Vulprit Prooze <raileyalin@gmail.com>
2025-10-05 19:19:37 +0800

    added registration [frontend]

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from '../common/Header'
 import MainContent from './MainContent'
 
--- frontend/src/interfaces/context/IAuthContext.ts
+++ frontend/src/interfaces/context/IAuthContext.ts
@@ -1,7 +1,10 @@
+import type { IUser, ILoginRequest, IRegisterRequest } from "@interfaces";
+
 export interface IAuthContext {
-    handleFetchUser,
-    handleLogin,
-    handleLogout,
-    user,
-    isLoading
+  handleFetchUser: () => Promise<IUser | null>;
+  handleLogin: (credentials: ILoginRequest) => Promise<void>;
+  handleLogout: () => void;
+  handleRegister: (credentials: IRegisterRequest) => Promise<void>;
+  user: IUser | null;
+  isLoading: boolean;
 }
