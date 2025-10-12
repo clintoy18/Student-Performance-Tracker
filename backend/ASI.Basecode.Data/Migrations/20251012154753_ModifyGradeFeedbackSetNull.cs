16acccdfbb76f31e72cdc8c9389e3c82e49997c2
Vulprit Prooze <raileyalin@gmail.com>
2025-10-12 23:52:12 +0800

    fixed critical issue with deleting student course [need to update database]

---
+++ backend/ASI.Basecode.Data/Migrations/20251012154753_ModifyGradeFeedbackSetNull.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASI.Basecode.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyGradeFeedbackSetNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GradeFeedbacks_StudentCourses_StudentCourseId",
+                table: "GradeFeedbacks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GradeFeedbacks_Users_UserId",
+                table: "GradeFeedbacks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GradeFeedbacks_StudentCourses_StudentCourseId",
+                table: "GradeFeedbacks",
+                column: "StudentCourseId",
+                principalTable: "StudentCourses",
+                principalColumn: "StudentCourseId",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GradeFeedbacks_Users_UserId",
+                table: "GradeFeedbacks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GradeFeedbacks_StudentCourses_StudentCourseId",
+                table: "GradeFeedbacks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GradeFeedbacks_Users_UserId",
+                table: "GradeFeedbacks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GradeFeedbacks_StudentCourses_StudentCourseId",
+                table: "GradeFeedbacks",
+                column: "StudentCourseId",
+                principalTable: "StudentCourses",
+                principalColumn: "StudentCourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GradeFeedbacks_Users_UserId",
+                table: "GradeFeedbacks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+    }
+}
