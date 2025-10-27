c8aec0ec7d0e21904f71c6289b08abf83ddca0e7
clintoy18 <aicsalonzo@gmail.com>
2025-10-27 22:01:09 +0800

    backend : generate dashboard summary. TBR : implement separate controller for  the pdf.

--- backend/ASI.Basecode.Services/Interfaces/IPdfService.cs
+++ backend/ASI.Basecode.Services/Interfaces/IPdfService.cs
@@ -5,5 +5,6 @@ public interface IPdfService
 {
     byte[] GenerateSimplePdf(string title, string message);
     void GenerateAndSavePdf(string filePath, string title, string message);
-    byte[] GenerateUserReport(UserStatisticsViewModel stats, List<UserViewAdminModel> users);
+    //byte[] GenerateUserReport(UserStatisticsViewModel stats, List<UserViewAdminModel> users);
+    byte[] GenerateDashboardSummaryReport(DashboardStatsViewModel dashboardStats, List<UserViewAdminModel> userLists);
 }
