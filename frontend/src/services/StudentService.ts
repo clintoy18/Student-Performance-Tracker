238fb93ed5ee57f983ccac032ab2b0a75e498cc6
Vulprit Prooze <raileyalin@gmail.com>
2025-11-09 17:21:18 +0800

    ss

--- frontend/src/services/StudentService.ts
+++ frontend/src/services/StudentService.ts
@@ -1,6 +1,14 @@
-import { studentCourse } from "lib/api"
+import type { IStudentFeedbackRequest } from "@interfaces/requests/IStudentFeedbackRequest"
+import { feedback, studentCourse } from "lib/api"
 
 export const fetchCoursesOfStudent = async(studentUserId: string) => {
     const response = await studentCourse.get(`student/${studentUserId}`)
+    return response.data
+}
+
+export const createStudentFeedback = async(request: IStudentFeedbackRequest) => {
+    const response = await feedback.put(`student/create`,
+        { request }
+    )
     return response.data
 }
